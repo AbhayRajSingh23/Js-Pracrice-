400e8236ca0f274ecb48319c74e5a63f261a3552
Abhay Raj Singh <148850248+AbhayRajSingh23@users.noreply.github.com>
2024-08-09 15:04:11 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2<=3);
+// console.log(2==3);
+// console.log(2!=3);
+
+// ********* tring to compare the differnt datatype************
+
+console.log("2">1);
+console.log("02">1); // Comparision converts the string to number to compaire
+//but the equality checks equality instead of comparing 
+
+console.log(num > 0);  //false
+console.log(num == 0); // false
+console.log(num >= 0); //conert the null value into the number
