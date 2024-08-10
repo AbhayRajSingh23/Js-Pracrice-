894ce1ddd45c8af2e9a411b42adaed6101dc6e11
Abhay Raj Singh <148850248+AbhayRajSingh23@users.noreply.github.com>
2024-08-10 20:43:23 +0000

    arrays part two

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -28,4 +28,4 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3)); //=> makes array of the fileds that are entered 
+console.log(Array.of(score1, score2, score3)); //=> makes array of the fileds that are entered.
