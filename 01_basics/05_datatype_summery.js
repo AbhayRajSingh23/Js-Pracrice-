9460e7cedbb2b2623654015201875136621d08a2
Abhay Raj Singh <148850248+AbhayRajSingh23@users.noreply.github.com>
2024-08-10 06:36:22 +0000

    Stack and Heap memory summary

--- 01_basics/05_datatype_summery.js
+++ 01_basics/05_datatype_summery.js
@@ -29,6 +29,7 @@ const myFunction = function(){      //function
 
 console.log("***********************************************")
 console.log(typeof myFunction)
+console.log("***********************************************")
 /* these are primitive
 // typeof null => object
 // typeof undefined => undefined
@@ -41,3 +42,28 @@ console.log(typeof myFunction)
 // typeof object => object
 // typeof function => function (object function)
  */
+
+//*************************************************************************************** */
+// Stack(Primitive) and heap(Non-Primitive) memomry.....
+
+//Primitive memory is call by value that means copy of value is passed when called
+let name = "Sighania";
+let anotherName =name;
+anotherName = "Rajpoot";
+console.log(name)
+console.log(anotherName)
+
+console.log("********************************")
+
+//Non-Primitvive memory is call by refernce value that means refernce of the is passed
+let userone ={
+    email : "jaibabaki@omshanti.com" ,
+    upi: "donate@jaiho"
+}
+let usertwo = userone;
+usertwo.email = "money@give"
+console.log(userone);
+console.log(usertwo);           //here the change in usertwo changes the userone details as it is in heap memory i.e. a refernce memory 
+                                                //(both the variable points the same refernce of variable) 
+
+
